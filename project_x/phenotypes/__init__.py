6e904012d81ca07661560b2de64d859d9829ff91
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2016-04-28 14:23:49 -0400

    Added a description of the available genotypes and phenotypes format, and the different statistical models.

---
+++ project_x/phenotypes/__init__.py
@@ -0,0 +1,19 @@
+"""
+"""
+
+
+# This file is part of project_x.
+#
+# This work is licensed under the Creative Commons Attribution-NonCommercial
+# 4.0 International License. To view a copy of this license, visit
+# http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to Creative
+# Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
+
+__copyright__ = "Copyright 2016, Beaulieu-Saucier Pharmacogenomics Centre"
+__license__ = "CC BY-NC 4.0"
+
+
+available_formats = dict(
+    text="Normal text file (TSV, CSV, etc).",
+)
---
+++ project_x/statistics/__init__.py
@@ -0,0 +1,22 @@
+"""
+"""
+
+
+# This file is part of project_x.
+#
+# This work is licensed under the Creative Commons Attribution-NonCommercial
+# 4.0 International License. To view a copy of this license, visit
+# http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to Creative
+# Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
+
+__copyright__ = "Copyright 2016, Beaulieu-Saucier Pharmacogenomics Centre"
+__license__ = "CC BY-NC 4.0"
+
+
+available_models = dict(
+    linear="Linear regression (ordinary least squares).",
+    logistic="Logistic regression (GLM with binomial distribution).",
+    mixedlm="Linear mixed effect model (random intercept).",
+    coxph="Cox's proportional hazard regression (survival analysis).",
+)
