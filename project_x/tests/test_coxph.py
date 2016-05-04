675e0123c039b9b23461fb41fe45a4e302e8f06b
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2016-05-04 08:46:02 -0400

    Added tests for CoxPH with interaction

--- project_x/tests/test_coxph.py
+++ project_x/tests/test_coxph.py
@@ -33,6 +33,20 @@ class TestStatsCoxPH(unittest.TestCase):
             interaction=None,
             normalize=False,
         )
+        cls.coxph_inter = StatsCoxPH(
+            time_to_event="tte",
+            event="event",
+            predictors=["age", "var1", "C(gender)", "geno"],
+            interaction="var1",
+            normalize=False,
+        )
+        cls.coxph_inter_cat = StatsCoxPH(
+            time_to_event="tte",
+            event="event",
+            predictors=["age", "var1", "C(gender)", "geno"],
+            interaction="C(gender)",
+            normalize=False,
+        )
 
     def setUp(self):
         self.data = pd.read_csv(
@@ -83,6 +97,82 @@ class TestStatsCoxPH(unittest.TestCase):
             -np.log10(self.coxph.results.p_value), places=2,
         )
 
+    def test_coxph_snp1_inter_full(self):
+        """Tests coxph regression with the first SNP (full, interaction)."""
+        # Preparing the data
+        pheno = self.data[["tte", "event", "age", "var1", "gender"]]
+        geno = self.data[["snp1"]].rename(columns={"snp1": "geno"})
+
+        # Permuting the genotypes
+        geno = geno.iloc[np.random.permutation(geno.shape[0]), :]
+
+        # Preparing the matrices
+        y, X = self.coxph_inter.create_matrices(pheno)
+
+        # Merging with genotype
+        y, X = self.coxph_inter.merge_matrices_genotypes(y, X, geno)
+
+        # Fitting
+        self.coxph_inter.fit(y, X)
+
+        # Checking the results (according to SAS)
+        # SAS doesn't compute a hazard ratio (and its 95% CI) for interaction
+        # (since it would be interpretable)
+        self.assertAlmostEqual(
+            0.047567074771854, self.coxph_inter.results.coef, places=4,
+        )
+        self.assertAlmostEqual(
+            0.06434824185209, self.coxph_inter.results.std_err, places=5,
+        )
+        self.assertAlmostEqual(
+            np.exp(0.047567074771854), self.coxph_inter.results.hr, places=4,
+        )
+        self.assertAlmostEqual(
+            0.54643626988504, self.coxph_inter.results.z_value**2, places=3,
+        )
+        self.assertAlmostEqual(
+            0.45977749951036, self.coxph_inter.results.p_value, places=3,
+        )
+
+    def test_coxph_snp1_inter_category_full(self):
+        """Tests coxph first SNP (full, interaction, category)."""
+        # Preparing the data
+        pheno = self.data[["tte", "event", "age", "var1", "gender"]]
+        geno = self.data[["snp1"]].rename(columns={"snp1": "geno"})
+
+        # Permuting the genotypes
+        geno = geno.iloc[np.random.permutation(geno.shape[0]), :]
+
+        # Preparing the matrices
+        y, X = self.coxph_inter_cat.create_matrices(pheno)
+
+        # Merging with genotype
+        y, X = self.coxph_inter_cat.merge_matrices_genotypes(y, X, geno)
+
+        # Fitting
+        self.coxph_inter_cat.fit(y, X)
+
+        # Checking the results (according to SAS)
+        # SAS doesn't compute a hazard ratio (and its 95% CI) for interaction
+        # (since it would be interpretable)
+        self.assertAlmostEqual(
+            -1.34428789715448, self.coxph_inter_cat.results.coef, places=3,
+        )
+        self.assertAlmostEqual(
+            1.7731990844019, self.coxph_inter_cat.results.std_err, places=5,
+        )
+        self.assertAlmostEqual(
+            np.exp(-1.34428789715448), self.coxph_inter_cat.results.hr,
+            places=4,
+        )
+        self.assertAlmostEqual(
+            0.57473756080668, self.coxph_inter_cat.results.z_value**2,
+            places=3,
+        )
+        self.assertAlmostEqual(
+            0.44838245349613, self.coxph_inter_cat.results.p_value, places=4,
+        )
+
     def test_coxph_snp1(self):
         """Tests coxph regression with the first SNP."""
         # Preparing the data
@@ -117,6 +207,37 @@ class TestStatsCoxPH(unittest.TestCase):
         self.assertAlmostEqual(
             -np.log10(0.0009402074852055),
             -np.log10(self.coxph.results.p_value), places=2,
+        )
+
+    def test_coxph_snp1_inter(self):
+        """Tests coxph regression with the first SNP (interaction)."""
+        # Preparing the data
+        data = self.data[["tte", "event", "age", "var1", "gender", "snp1"]]
+        data = data.rename(columns={"snp1": "geno"})
+
+        # Preparing the matrices
+        y, X = self.coxph_inter.create_matrices(data, create_dummy=False)
+
+        # Fitting
+        self.coxph_inter.fit(y, X)
+
+        # Checking the results (according to SAS)
+        # SAS doesn't compute a hazard ratio (and its 95% CI) for interaction
+        # (since it would be interpretable)
+        self.assertAlmostEqual(
+            0.047567074771854, self.coxph_inter.results.coef, places=4,
+        )
+        self.assertAlmostEqual(
+            0.06434824185209, self.coxph_inter.results.std_err, places=5,
+        )
+        self.assertAlmostEqual(
+            np.exp(0.047567074771854), self.coxph_inter.results.hr, places=4,
+        )
+        self.assertAlmostEqual(
+            0.54643626988504, self.coxph_inter.results.z_value**2, places=3,
+        )
+        self.assertAlmostEqual(
+            0.45977749951036, self.coxph_inter.results.p_value, places=3,
         )
 
     def test_coxph_snp2(self):
@@ -155,6 +276,37 @@ class TestStatsCoxPH(unittest.TestCase):
             -np.log10(self.coxph.results.p_value), places=6,
         )
 
+    def test_coxph_snp2_inter(self):
+        """Tests coxph regression with the second SNP (interaction)."""
+        # Preparing the data
+        data = self.data[["tte", "event", "age", "var1", "gender", "snp2"]]
+        data = data.rename(columns={"snp2": "geno"})
+
+        # Preparing the matrices
+        y, X = self.coxph_inter.create_matrices(data, create_dummy=False)
+
+        # Fitting
+        self.coxph_inter.fit(y, X)
+
+        # Checking the results (according to SAS)
+        # SAS doesn't compute a hazard ratio (and its 95% CI) for interaction
+        # (since it would be interpretable)
+        self.assertAlmostEqual(
+            0.0174236370729153, self.coxph_inter.results.coef, places=5,
+        )
+        self.assertAlmostEqual(
+            0.0221904396707763, self.coxph_inter.results.std_err,
+        )
+        self.assertAlmostEqual(
+            np.exp(0.0174236370729153), self.coxph_inter.results.hr, places=5,
+        )
+        self.assertAlmostEqual(
+            0.6165180814095, self.coxph_inter.results.z_value**2, places=4,
+        )
+        self.assertAlmostEqual(
+            0.4323441392819500, self.coxph_inter.results.p_value, places=4,
+        )
+
     def test_coxph_snp3(self):
         """Tests coxph regression with the third SNP."""
         # Preparing the data
@@ -190,6 +342,37 @@ class TestStatsCoxPH(unittest.TestCase):
             0.98609286353578, self.coxph.results.p_value, places=4,
         )
 
+    def test_coxph_snp3_inter(self):
+        """Tests coxph regression with the third SNP (interaction)."""
+        # Preparing the data
+        data = self.data[["tte", "event", "age", "var1", "gender", "snp3"]]
+        data = data.rename(columns={"snp3": "geno"})
+
+        # Preparing the matrices
+        y, X = self.coxph_inter.create_matrices(data, create_dummy=False)
+
+        # Fitting
+        self.coxph_inter.fit(y, X)
+
+        # Checking the results (according to SAS)
+        # SAS doesn't compute a hazard ratio (and its 95% CI) for interaction
+        # (since it would be interpretable)
+        self.assertAlmostEqual(
+            0.0058742328153452, self.coxph_inter.results.coef, places=5,
+        )
+        self.assertAlmostEqual(
+            0.0335085402386551, self.coxph_inter.results.std_err,
+        )
+        self.assertAlmostEqual(
+            np.exp(0.0058742328153452), self.coxph_inter.results.hr, places=5,
+        )
+        self.assertAlmostEqual(
+            0.0307320331310296, self.coxph_inter.results.z_value**2, places=4,
+        )
+        self.assertAlmostEqual(
+            0.86083955715083, self.coxph_inter.results.p_value, places=4,
+        )
+
     def test_coxph_snp4(self):
         """Tests coxph regression with the third SNP."""
         # Preparing the data
@@ -211,3 +394,25 @@ class TestStatsCoxPH(unittest.TestCase):
         self.assertTrue(np.isnan(self.coxph.results.hr_upper_ci))
         self.assertTrue(np.isnan(self.coxph.results.z_value))
         self.assertTrue(np.isnan(self.coxph.results.p_value))
+
+    def test_coxph_snp4_inter(self):
+        """Tests coxph regression with the third SNP (interaction)."""
+        # Preparing the data
+        data = self.data[["tte", "event", "age", "var1", "gender", "snp4"]]
+        data = data.rename(columns={"snp4": "geno"})
+
+        # Preparing the matrices
+        y, X = self.coxph_inter.create_matrices(data, create_dummy=False)
+
+        # Fitting
+        with self.assertRaises(StatsError):
+            self.coxph_inter.fit(y, X)
+
+        # Checking the results (according to SAS)
+        self.assertTrue(np.isnan(self.coxph_inter.results.coef))
+        self.assertTrue(np.isnan(self.coxph_inter.results.std_err))
+        self.assertTrue(np.isnan(self.coxph_inter.results.hr))
+        self.assertTrue(np.isnan(self.coxph_inter.results.hr_lower_ci))
+        self.assertTrue(np.isnan(self.coxph_inter.results.hr_upper_ci))
+        self.assertTrue(np.isnan(self.coxph_inter.results.z_value))
+        self.assertTrue(np.isnan(self.coxph_inter.results.p_value))
