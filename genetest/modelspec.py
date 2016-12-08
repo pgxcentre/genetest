4777805a7a8bbc96cf2fd23585221ebff7f2bad7
Marc André Legault <legaultmarc@gmail.com>
2016-12-08 14:56:50 -0500

    Added symbolic arithmetic to make it possible to do compute GRS and other affine transformations on the fly.

--- genetest/modelspec.py
+++ genetest/modelspec.py
@@ -6,12 +6,108 @@ Utilities to build statistical models.
 # use in the ModelSpec creation.
 
 import uuid
+import collections
+import operator
+
+import numpy as np
 
 from .statistics import available_models
 
 
+class transformation_handler(object):
+    handlers = {}
+
+    def __init__(self, action):
+        self.action = action
+
+    def __call__(self, f):
+        self.__class__.handlers[self.action] = f
+        return f
+
+
 class SNPs(object):
     pass
+
+
+class Expression(object):
+    def __init__(self, expression):
+        self.expression = expression
+
+    def __mul__(self, other):
+        return Expression((operator.mul, self.expression, other))
+
+    def __div__(self, other):
+        return Expression((operator.div, self.expression, other))
+
+    def __add__(self, other):
+        return Expression((operator.add, self.expression, other))
+
+    def __sub__(self, other):
+        return Expression((operator.sub, self.expression, other))
+
+    __rmul__ = __mul__
+    __rdiv__ = __div__
+    __radd__ = __add__
+    __rsub__ = __sub__
+
+    def eval(self):
+        return self._eval_expression(self.expression)
+
+    @staticmethod
+    def _eval_expression(expression):
+        op, left, right = expression
+
+        if isinstance(left, tuple):
+            left = Expression._eval_expression(left)
+
+        if isinstance(left, Expression):
+            left = Expression._eval_expression(left.expression)
+
+        if isinstance(right, tuple):
+            right = Expression._eval_expression(right)
+
+        if isinstance(right, Expression):
+            right = Expression._eval_expression(right.expression)
+
+        if hasattr(left, "values"):
+            left = left.values
+
+        if hasattr(right, "values"):
+            right = right.values
+
+        return op(left, right)
+
+
+class EntityIdentifier(object):
+    def __init__(self, id=None):
+        if id:
+            self.id = id
+        else:
+            self.id = str(uuid.uuid4())
+        self.values = None
+
+    def __mul__(self, other):
+        return Expression((operator.mul, self, other))
+
+    def __div__(self, other):
+        return Expression((operator.div, self, other))
+
+    def __add__(self, other):
+        return Expression((operator.add, self, other))
+
+    def __sub__(self, other):
+        return Expression((operator.sub, self, other))
+
+    __rmul__ = __mul__
+    __rdiv__ = __div__
+    __radd__ = __add__
+    __rsub__ = __sub__
+
+    def __repr__(self):
+        return self.id.split("-")[0]
+
+    def bind(self, values):
+        self.values = values
 
 
 class DependencyManager(object):
@@ -41,10 +137,13 @@ class TransformationManager(object):
     def __init__(self, action):
         self.action = action
 
-    def __call__(self, target, *params):
+    def __call__(self, source, *params):
+        # Generate an identifier for the result of the transformation.
+        target = str(uuid.uuid4())
         TransformationManager.transformations.append(
-            (self.action, target, params)
+            (self.action, source, target, params)
         )
+        return target
 
 
 class ModelSpec(object):
@@ -89,9 +188,77 @@ class ModelSpec(object):
     def transformations(self):
         return TransformationManager.transformations
 
+    def create_data_matrix(self, phenotypes, genotypes):
+        """Create the data matrix given data containers.
+
+        Args:
+            phenotypes (PhenotypeContainer): The PhenotypeContainer instance.
+            genotypes (GenotypeContainer): The GenotypeContainer instance.
+
+        """
+        # Extract the phenotype dependencies.
+        PHENOTYPES = "PHENOTYPES"
+
+        phen_keys = [
+            k[1] for k, v in self.dependencies.items() if k[0] == PHENOTYPES
+        ]
+
+        df = phenotypes.get_phenotypes(phen_keys)
+        df.columns = [self.dependencies[(PHENOTYPES, k)] for k in phen_keys]
+
+        # Extract the genotype dependencies.
+        # TODO
+        # Also handle SNPs.
+
+        # Apply transformations.
+        for action, source, target, params in self.transformations:
+            f = transformation_handler.handlers[action]
+            res = f(df, source, *params)
+
+            # Some tranformations return multiple columns. We create all the
+            # relevant columns in the dataframe.
+            if isinstance(res, collections.Iterable):
+                for i, col in enumerate(res):
+                    df["{}-{}".format(target, i + 1)] = col
+
+            # In most cases, transformations return a single array. We set it
+            # under the target ID.
+            else:
+                df[target] = res
+
+        print(df)
+        return df
+
+
+@transformation_handler("LOG10")
+def _log10(data, column):
+    return np.log10(data[column])
+
+
+@transformation_handler("ENCODE_FACTOR")
+def _encode_factor(data, column):
+    # TODO
+    return data[column]
+
+
+@transformation_handler("POW")
+def _pow(data, column, power):
+    return np.pow(data[column], power)
+
+
+@transformation_handler("INTERACTION")
+def _interaction(data, column, interaction_target):
+    return data[column] * data[interaction_target]
+
+
+@transformation_handler("GENETIC_RISK_SCORE")
+def _grs(data, column):
+    # TODO
+    return data[column]
 
 phenotypes = DependencyManager("PHENOTYPES")
 genotypes = DependencyManager("GENOTYPES")
+
 factor = TransformationManager("ENCODE_FACTOR")
 log10 = TransformationManager("LOG10")
 pow = TransformationManager("POW")
---
+++ genetest/phenotypes/dummy.py
@@ -0,0 +1,61 @@
+"""
+Dummy implementation of the phenotype container with test data.
+"""
+
+# This file is part of genetest.
+#
+# This work is licensed under the Creative Commons Attribution-NonCommercial
+# 4.0 International License. To view a copy of this license, visit
+# http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to Creative
+# Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
+
+import pandas as pd
+import numpy as np
+
+from ..decorators import arguments
+from .core import PhenotypesContainer
+
+
+__copyright__ = "Copyright 2016, Beaulieu-Saucier Pharmacogenomics Centre"
+__license__ = "Attribution-NonCommercial 4.0 International (CC BY-NC 4.0)"
+
+
+__all__ = ["_DummyPhenotypes"]
+
+
+@arguments(required=(), optional={})
+class _DummyPhenotypes(PhenotypesContainer):
+    def __init__(self):
+        self.data = pd.DataFrame({
+            "y": [0, 1, 0, np.nan, 0],
+            "V1": [1, 2, 3, 4, 5],
+            "V2": [0, np.nan, 1, 0, 1],
+            "V3": [-2, 1, np.nan, 0, -1],
+            "V4": [0.1, 0.2, 0.3, 0.4, 0.5],
+        }, index=["s{}".format(i) for i in range(1, 6)])
+
+    def close(self):
+        pass
+
+    def get_phenotypes(self, li=None):
+        for i in li:
+            if i not in self.data.columns:
+                raise KeyError(i)
+
+        if li is None:
+            li = self.data.columns
+
+        return self.data.loc[:, list(li)]
+
+    def get_nb_samples(self):
+        return self.data.shape[0]
+
+    def get_nb_variables(self):
+        return self.data.shape[1]
+
+    def is_repeated(self):
+        return False
+
+    def keep_samples(self, keep):
+        self.data = self.data.loc[keep, :]
