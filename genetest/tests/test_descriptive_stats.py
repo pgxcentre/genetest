6e7bfd1dffbb0404257fe39ed24cb9f664f2f68f
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2016-12-20 14:14:08 -0500

    Updated the tests for the descriptive statistics

--- genetest/tests/test_descriptive_stats.py
+++ genetest/tests/test_descriptive_stats.py
@@ -20,23 +20,49 @@ __copyright__ = "Copyright 2016, Beaulieu-Saucier Pharmacogenomics Centre"
 __license__ = "Attribution-NonCommercial 4.0 International (CC BY-NC 4.0)"
 
 
-class TestdescriptiveStats(unittest.TestCase):
+class TestDescriptiveStats(unittest.TestCase):
     """Tests the descriptive statistics module."""
-    def test_get_freq_without_null(self):
+    def test_get_maf_without_null(self):
         """Tests when there are no missing genotypes."""
         genotypes = pd.Series([0, 0, 1, 0, 1, 0, 2, 0, 1, 0])
-        freq = descriptive.get_freq(genotypes)
-        self.assertAlmostEqual(5 / 20, freq, places=10)
+        maf, minor, major, flip = descriptive.get_maf(
+            genotypes, "A", "B",
+        )
+        self.assertAlmostEqual(5 / 20, maf, places=10)
+        self.assertEqual("A", minor)
+        self.assertEqual("B", major)
+        self.assertFalse(flip)
 
-    def test_get_freq_with_null(self):
+    def test_get_maf_flip(self):
+        """Tests when there are no missing genotypes."""
+        genotypes = pd.Series([2, 2, 1, 2, 1, 2, 0, 2, 1, 2])
+        maf, minor, major, flip = descriptive.get_maf(
+            genotypes, "A", "B",
+        )
+        self.assertAlmostEqual(5 / 20, maf, places=10)
+        self.assertEqual("B", minor)
+        self.assertEqual("A", major)
+        self.assertTrue(flip)
+
+    def test_get_maf_with_null(self):
         """Tests when there are missing genotypes."""
         genotypes = pd.Series([0, 0, 1, 0, np.nan, 0, np.nan, 0, 1, 0])
-        freq = descriptive.get_freq(genotypes)
-        self.assertAlmostEqual(2 / 16, freq, places=10)
+        maf, minor, major, flip = descriptive.get_maf(
+            genotypes, "A", "B",
+        )
+        self.assertAlmostEqual(2 / 16, maf, places=10)
+        self.assertEqual("A", minor)
+        self.assertEqual("B", major)
+        self.assertFalse(flip)
 
-    def test_get_freq_no_valid_genotypes(self):
+    def test_get_maf_no_valid_genotypes(self):
         """Tests when all genotypes are missing."""
         genotypes = pd.Series([np.nan, np.nan, np.nan, np.nan, np.nan, np.nan,
                                np.nan, np.nan, np.nan, np.nan])
-        freq = descriptive.get_freq(genotypes)
-        self.assertTrue(np.isnan(freq))
+        maf, minor, major, flip = descriptive.get_maf(
+            genotypes, "A", "B",
+        )
+        self.assertTrue(np.isnan(maf))
+        self.assertEqual("A", minor)
+        self.assertEqual("B", major)
+        self.assertFalse(flip)
