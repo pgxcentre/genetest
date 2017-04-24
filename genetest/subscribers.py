a5622dc8d6c26aca529b399dbde524c5b8f5d068
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2017-04-24 15:02:33 -0400

    Added a GWAS subscriber

--- genetest/subscribers.py
+++ genetest/subscribers.py
@@ -15,6 +15,11 @@ import sys
 import pprint
 import logging
 
+from .modelspec import result as analysis_results
+
+
+__all__ = ["Print", "ResultsMemory", "RowWriter", "GWASWriter"]
+
 
 logger = logging.getLogger(__name__)
 
@@ -134,6 +139,78 @@ class RowWriter(Subscriber):
             self._f.flush()
         else:
             print(row)
+
+
+class GWASWriter(RowWriter):
+    def __init__(self, filename, test, sep="\t"):
+        # The columns that are always present
+        columns = [
+            ("snp", analysis_results["SNPs"]["name"]),
+            ("chr", analysis_results["SNPs"]["chrom"]),
+            ("pos", analysis_results["SNPs"]["pos"]),
+            ("major", analysis_results["SNPs"]["major"]),
+            ("minor", analysis_results["SNPs"]["minor"]),
+            ("maf", analysis_results["SNPs"]["maf"]),
+            ("n", analysis_results["MODEL"]["nobs"]),
+        ]
+
+        # The columns required by the 'linear' test
+        if test == "linear":
+            columns.extend([
+                ("coef", analysis_results["SNPs"]["coef"]),
+                ("se", analysis_results["SNPs"]["std_err"]),
+                ("lower", analysis_results["SNPs"]["lower_ci"]),
+                ("upper", analysis_results["SNPs"]["upper_ci"]),
+                ("t", analysis_results["SNPs"]["t_value"]),
+                ("p", analysis_results["SNPs"]["p_value"]),
+                ("ll", analysis_results["MODEL"]["log_likelihood"]),
+                ("adj_r2", analysis_results["MODEL"]["r_squared_adj"]),
+            ])
+
+        # The columns required by the 'logistic' test
+        elif test == "logistic":
+            columns.extend([
+                ("coef", analysis_results["SNPs"]["coef"]),
+                ("se", analysis_results["SNPs"]["std_err"]),
+                ("lower", analysis_results["SNPs"]["lower_ci"]),
+                ("upper", analysis_results["SNPs"]["upper_ci"]),
+                ("t", analysis_results["SNPs"]["t_value"]),
+                ("p", analysis_results["SNPs"]["p_value"]),
+                ("ll", analysis_results["MODEL"]["log_likelihood"]),
+            ])
+
+        # The columns required by the 'coxph' test
+        elif test == "coxph":
+            columns.extend([
+                ("coef", analysis_results["SNPs"]["coef"]),
+                ("se", analysis_results["SNPs"]["std_err"]),
+                ("hr", analysis_results["SNPs"]["hr"]),
+                ("hr_lower", analysis_results["SNPs"]["hr_lower_ci"]),
+                ("hr_upper", analysis_results["SNPs"]["hr_upper_ci"]),
+                ("z", analysis_results["SNPs"]["z_value"]),
+                ("p", analysis_results["SNPs"]["p_value"]),
+                ("ll", analysis_results["MODEL"]["log_likelihood"]),
+            ])
+
+        # The columns required by the 'coxph' test
+        elif test == "mixedlm":
+            columns.extend([
+                ("coef", analysis_results["SNPs"]["coef"]),
+                ("se", analysis_results["SNPs"]["std_err"]),
+                ("lower", analysis_results["SNPs"]["lower_ci"]),
+                ("upper", analysis_results["SNPs"]["upper_ci"]),
+                ("z", analysis_results["SNPs"]["z_value"]),
+                ("p", analysis_results["SNPs"]["p_value"]),
+                ("ll", analysis_results["MODEL"]["log_likelihood"]),
+            ])
+
+        else:
+            logger.warning("{}: invalid test: only common columns will be "
+                           "written to file.")
+
+        # Calling super __init__
+        super().__init__(filename=filename, columns=columns, header=True,
+                         append=False, sep=sep)
 
 
 def subscriber_error(message, abort=None):
